171eabfbe98f9da4565f073364b9720ec482e73e
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2024-03-05 13:08:50 -0300

    Add unit tests for book module models

--- BitsoBookModule/Tests/Tests/Model/BookDetailsTests.swift
+++ BitsoBookModule/Tests/Tests/Model/BookDetailsTests.swift
@@ -1,8 +1,83 @@
-//
-//  File.swift
-//  
-//
-//  Created by Igor on 05/03/2024.
-//
+import XCTest
+@testable import BitsoBookModule
 
-import Foundation
+final class BookDetailsTests: XCTestCase {
+    private var decoder: JSONDecoder!
+
+    override func setUp() {
+        super.setUp()
+        decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .iso8601
+    }
+
+    func test_decode_withValidData() throws {
+        let jsonString = """
+        {
+            "book": "btc_mxn",
+            "created_at": "2023-03-09T20:58:23+00:00",
+            "high": "472472.82",
+            "last": "372110.00",
+            "volume": "112.81964756",
+            "vwap": "388387.4631589659",
+            "low": "10000.00",
+            "ask": "372800.00",
+            "bid": "372110.00",
+            "change_24": "-25580.00"
+        }
+        """
+
+        let data = Data(jsonString.utf8)
+        let model = try decoder.decode(BookDetails.self, from: data)
+
+        XCTAssertEqual(model.name, "btc_mxn")
+        XCTAssertEqual(model.high, "472472.82")
+        XCTAssertEqual(model.last, "372110.00")
+        XCTAssertEqual(model.volume, "112.81964756")
+        XCTAssertEqual(model.vwap, "388387.4631589659")
+        XCTAssertEqual(model.low, "10000.00")
+        XCTAssertEqual(model.ask, "372800.00")
+        XCTAssertEqual(model.bid, "372110.00")
+        XCTAssertEqual(model.change, "-25580.00")
+    }
+
+    func test_decode_withInvalidDate() {
+        let jsonString = """
+        {
+            "book": "btc_mxn",
+            "created_at": "2023-03-09 20:58:23",
+            "high": "472472.82",
+            "last": "372110.00",
+            "volume": "112.81964756",
+            "vwap": "388387.4631589659",
+            "low": "10000.00",
+            "ask": "372800.00",
+            "bid": "372110.00",
+            "change_24": "-25580.00"
+        }
+        """
+
+        let data = Data(jsonString.utf8)
+        let model = try? decoder.decode(BookDetails.self, from: data)
+        XCTAssertNil(model)
+    }
+
+    func test_decode_withMissingData() {
+        let jsonString = """
+        {
+            "book": "btc_mxn",
+            "created_at": "2023-03-09T20:58:23+00:00",
+            "high": "472472.82",
+            "last": "372110.00",
+            "volume": "112.81964756",
+            "vwap": "388387.4631589659",
+            "low": "10000.00",
+            "ask": "372800.00",
+            "bid": "372110.00"
+        }
+        """
+
+        let data = Data(jsonString.utf8)
+        let model = try? decoder.decode(BookDetails.self, from: data)
+        XCTAssertNil(model)
+    }
+}
--- BitsoBookModule/Tests/Tests/Model/BookTests.swift
+++ BitsoBookModule/Tests/Tests/Model/BookTests.swift
@@ -1,8 +1,79 @@
-//
-//  File.swift
-//  
-//
-//  Created by Igor on 05/03/2024.
-//
+import XCTest
+@testable import BitsoBookModule
 
-import Foundation
+final class BookTests: XCTestCase {
+    private var decoder: JSONDecoder!
+
+    override func setUp() {
+        super.setUp()
+        decoder = JSONDecoder()
+    }
+
+    func test_decode_withValidData() throws {
+        let jsonString = """
+          {
+            "book":"eth_btc",
+            "default_chart":"candle",
+            "minimum_price":"0.00000100",
+            "maximum_price":"5000.00000000",
+            "minimum_value":"0.00000100",
+            "maximum_value":"2000.00000000",
+            "maximum_amount":"1000.00000000",
+            "minimum_amount":"0.00000100",
+            "tick_size":"0.00000001"
+        }
+        """
+
+        let data = Data(jsonString.utf8)
+        let book = try decoder.decode(Book.self, from: data)
+
+        XCTAssertEqual(book.name, "eth_btc")
+        XCTAssertEqual(book.defaultChart, .candle)
+        XCTAssertEqual(book.minimumPrice, "0.00000100")
+        XCTAssertEqual(book.maximumPrice, "5000.00000000")
+        XCTAssertEqual(book.minimumValue, "0.00000100")
+        XCTAssertEqual(book.maximumValue, "2000.00000000")
+        XCTAssertEqual(book.maximumAmount, "1000.00000000")
+        XCTAssertEqual(book.minimumAmount, "0.00000100")
+        XCTAssertEqual(book.tickSize, "0.00000001")
+    }
+
+    func test_decode_withInvalidChart() throws {
+        let jsonString = """
+          {
+            "book":"eth_btc",
+            "default_chart":"invalid",
+            "minimum_price":"0.00000100",
+            "maximum_price":"5000.00000000",
+            "minimum_value":"0.00000100",
+            "maximum_value":"2000.00000000",
+            "maximum_amount":"1000.00000000",
+            "minimum_amount":"0.00000100",
+            "tick_size":"0.00000001"
+        }
+        """
+
+        let data = Data(jsonString.utf8)
+        let book = try? decoder.decode(Book.self, from: data)
+        XCTAssertNil(book)
+    }
+
+    func test_decode_withMissingData() throws {
+        let jsonString = """
+          {
+            "book":"eth_btc",
+            "default_chart":"candle",
+            "minimum_price":"0.00000100",
+            "maximum_price":"5000.00000000",
+            "minimum_value":"0.00000100",
+            "maximum_value":"2000.00000000",
+            "maximum_amount":"1000.00000000",
+            "minimum_amount":"0.00000100"
+        }
+        """
+
+        let data = Data(jsonString.utf8)
+        let book = try? decoder.decode(Book.self, from: data)
+        XCTAssertNil(book)
+    }
+}
